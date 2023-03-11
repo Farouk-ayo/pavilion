4ee45e8f7a337650137aaf77b604047379c65e38
Farouk Mustapha <mustaphafarouk41@gmail.com>
2023-03-11 10:41:19 +0100

    applied the color to the button onclick

--- src/components/ButtonTabs.jsx
+++ src/components/ButtonTabs.jsx
@@ -2,18 +2,43 @@ import { useState } from "react";
 import classes from "./ButtonTabs.module.scss";
 
 const ButtonTabs = () => {
-  const [isActive, setActive] = useState(true);
-  const changeBlue = () => {};
+  const [isActive, setActive] = useState("blue");
+  const handleClick = (e) => {
+    setActive(e.target.value);
+  };
 
   return (
-    <div className={classes.btnGroup}>
-      <button className={classes.btnBlue} onClick={changeBlue}>
+    <div className={classes.btnGroup} onClick={handleClick}>
+      <button
+        className={isActive === "blue" ? classes.btnBlue : classes.btn}
+        value="blue"
+      >
         Cardholder
       </button>
-      <button className={classes.btn}>Acquirer</button>
-      <button className={classes.btn}>Issuer</button>
-      <button className={classes.btn}>Switch</button>
-      <button className={classes.btn}>Undefined</button>
+      <button
+        className={isActive === "red" ? classes.btnRed : classes.btn}
+        value="red"
+      >
+        Acquirer
+      </button>
+      <button
+        className={isActive === "purple" ? classes.btnPurple : classes.btn}
+        value="purple"
+      >
+        Issuer
+      </button>
+      <button
+        className={isActive === "orange" ? classes.btnOrange : classes.btn}
+        value="orange"
+      >
+        Switch
+      </button>
+      <button
+        className={isActive === "green" ? classes.btnGreen : classes.btn}
+        value="green"
+      >
+        Undefined
+      </button>
     </div>
   );
 };
